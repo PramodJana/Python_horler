9610290628e2a1f9f7008715a371fcfb89e1d686
PramodJana <pramodjana395@gmail.com>
2018-12-09 21:36:40 +0530

    oops_concept

--- emp.py
+++ emp.py
@@ -1,4 +1,8 @@
 class Employee:
+    raise_amount=1.04
+
+
+
     def __init__(self,first,last,email,pay):
         self.first=first
         self.last=last
@@ -8,12 +12,12 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay=int(self.pay *self.raise_amount)
 
 emp_1= Employee("Pramod","Jana","Pramodjana395@gmail.com",50000)
 emp_2= Employee("Shivani","Konar","mamonikonar512@gmail.com",60000)
 
-print(emp_1.email)
-print(emp_2.email)
-
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
