eafe2292f0361a837a4748ca9929f46a533b186b
PramodJana <pramodjana395@gmail.com>
2018-12-20 20:31:40 +0530

    operators and operands

---
+++ aritmaticoperator.py
@@ -0,0 +1,8 @@
+a,b =10,5
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+print("Floor Division: ",a//b)
+print("Modulus: ",a%b)
+print("power: ",a**b)
